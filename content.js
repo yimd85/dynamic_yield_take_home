4da3025395f14102b3db2f63469291e7efcca53d
yimd85 <yimd85@gmail.com>
2019-09-05 23:10:45 -0400

    add to bag increase and reordering

--- content.js
+++ content.js
@@ -1,14 +1,20 @@
-alert("test 1.5!");
-
 var clickedBox = document.getElementsByClassName(
   "c-product-tile__image-link js-product-tile__image-link"
 );
 var numClickedBox = clickedBox.length;
+var addToBagClicked = document.getElementsByClassName("c-pwa-add-to-bag");
+var numAddToBag = addToBagClicked.length;
 
 if (localStorage.getItem("CSE_Challenge") === null) {
   localStorage.setItem(
     "CSE_Challenge",
-    JSON.stringify({ [`Men's`]: 0, [`Women's`]: 0, Beauty: 0, Lifestyle: 0, Home: 0 })
+    JSON.stringify({
+      [`Men's`]: 0,
+      [`Women's`]: 0,
+      Beauty: 0,
+      Lifestyle: 0,
+      Home: 0
+    })
   );
 }
 
@@ -16,7 +22,7 @@ for (var i = 0; i < numClickedBox; i++) {
   clickedBox[i].className += ` ${clickedBox[i].dataset.gaEventAction}`;
   clickedBox[i].addEventListener("click", function(event) {
     localStorage.setItem(
-      "clicked_on",
+      "CSE_clicked_on",
       event.target.parentElement.className
         .replace(
           "c-product-tile__image-link js-product-tile__image-link product tray|",
@@ -29,14 +35,62 @@ for (var i = 0; i < numClickedBox; i++) {
 
 if (
   window.location.href.indexOf("https://www.urbanoutfitters.com/shop/") > -1 &&
-  localStorage.getItem("clicked_on") !== null
+  localStorage.getItem("CSE_clicked_on") !== null
 ) {
   var value_to_be_updated = JSON.parse(localStorage.getItem("CSE_Challenge"))[
-    `${localStorage.getItem("clicked_on")}`
+    `${localStorage.getItem("CSE_clicked_on")}`
   ];
+
   var final_tally = Object.assign(
     JSON.parse(localStorage.getItem("CSE_Challenge")),
-    { [`${localStorage.getItem("clicked_on")}`]: value_to_be_updated + 1 }
+    { [`${localStorage.getItem("CSE_clicked_on")}`]: value_to_be_updated + 1 }
   );
+
   localStorage.setItem("CSE_Challenge", JSON.stringify(final_tally));
 }
+
+for (var i = 0; i < numAddToBag; i++) {
+  var add_to_be_update = JSON.parse(localStorage.getItem("CSE_Challenge"))[
+    `${localStorage.getItem("CSE_clicked_on")}`
+  ];
+
+  var final_tally_added = Object.assign(
+    JSON.parse(localStorage.getItem("CSE_Challenge")),
+    { [`${localStorage.getItem("CSE_clicked_on")}`]: add_to_be_update + 3 }
+  );
+  addToBagClicked[i].addEventListener("click", function(event) {
+    localStorage.setItem("CSE_Challenge", JSON.stringify(final_tally_added));
+  });
+}
+
+document.getElementsByClassName("dom-landing-page-modules")[0].style.display =
+  "flex";
+document.getElementsByClassName("dom-landing-page-modules")[0].style[
+  "flex-direction"
+] = "column";
+
+document.getElementsByClassName("o-row")[33].style.order = -JSON.parse(
+  localStorage.getItem("CSE_Challenge")
+)[`Women's`];
+document.getElementsByClassName("o-row")[48].style.order = -JSON.parse(
+  localStorage.getItem("CSE_Challenge")
+)[`Men's`];
+
+document.getElementsByClassName("o-row")[63].style.order = -JSON.parse(
+  localStorage.getItem("CSE_Challenge")
+)[`Home`];
+
+document.getElementsByClassName("o-row")[78].style.order = -JSON.parse(
+  localStorage.getItem("CSE_Challenge")
+)[`Lifestyle`];
+
+document.getElementsByClassName("o-row")[93].style.order = -JSON.parse(
+  localStorage.getItem("CSE_Challenge")
+)[`Beauty`];
+
+document.getElementsByClassName(
+  "o-row s-new-arrivals-us-ca-hero  "
+)[0].style.order = -10000;
+
+
+
